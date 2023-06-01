5545fae9db38da1c455d6d4dfb0e546b1e7d7a4d
stefanoschoonheimMBO <320791@student.mboutrecht.nl>
2023-06-01 15:11:27 +0200

    added link to home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -11,6 +11,7 @@
 <body>
     <h3><?= $data['title']; ?></h3>
     <a href="<?= URLROOT; ?>/instructeur/index">Instructeurs in dienst</a>
+    <a href="<?= URLROOT; ?>/exames/index">Overzicht Afgenomen Examens Examinatoren</a>
 </body>
 
 </html>
